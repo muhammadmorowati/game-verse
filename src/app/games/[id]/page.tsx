323b14d3e6c3d9aec3110c7ffa6239a749c15330
mh-morowati <muhammadmorowati@gmail.com>
2025-08-29 03:25:08 -1200

    feat: add routing and pages

---
+++ src/app/games/[id]/page.tsx
@@ -0,0 +1,5 @@
+const GameDetails = () => {
+    return(<>Games</>)
+}
+
+export default GameDetails
---
+++ src/app/games/page.tsx
@@ -0,0 +1,5 @@
+const GamesListing = () => {
+    return(<>GamesListing</>)
+}
+
+export default GamesListing
